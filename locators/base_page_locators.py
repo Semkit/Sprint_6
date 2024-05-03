e46cff2cabed0ce0c70e102bd485f1130178233d
Semkit <semkit23@gmail.com>
2024-05-03 21:07:54 +0300

    initial commit

---
+++ locators/base_page_locators.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+class BasePageLocators:
+    YANDEX_LOGO = (By.XPATH, '//a[@class="Header_LogoYandex__3TSOI"]')
+    SCOOTER_LOGO = (By.XPATH, '//a[@class="Header_LogoScooter__3lsAR"]')
+    ACCEPT_COOKIE = (By.XPATH, "//*[contains(@class,'App_CookieButton__3cvqF')]")
+    UPPER_ORDER_BUTTON = (By.XPATH, "//button[@class='Button_Button__ra12g']")
